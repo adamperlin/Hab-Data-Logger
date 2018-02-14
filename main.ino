63a8c244c846918155e0f08748eacb40ff9516a4
adamperlin <adamperlin@seattleacademy.org>
2018-02-13 19:55:23 -0800

    Add test for reading from SD card, refactor polling function

--- main.ino
+++ main.ino
@@ -2,7 +2,6 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <Adafruit_MPL3115A2.h>
-
 #include <utility/imumaths.h>
 #include <SD.h>
 
@@ -11,8 +10,8 @@
  */
 #define SD_PIN 10 //can change
 #define IMU_SENSOR_ID 55
-
-#define IMU_DATA_FILE "data.txt"
+#define SD_DATA_FILE "data.txt"
+#define SEATTLE_BASELINE_ATM 102710.0
 
 
 /*
@@ -30,25 +29,30 @@ void setup() {
   Serial.println("Beginning initialization...");
   devicesInit();
   Serial.println("Initialization Finished");
+
+  for (int i = 0; i < 10; ++i) {
+      pollSensors();
+      delay(1000);
+  }
+
+  Serial.println("Test data read: ");
+  testDataRead();
+  
   delay(1000);
 }
 
-long currentTime = 0L;
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  pollSensors();
-  checkAltimeter();
-  // Do any other necessary repetitive tasks
-  currentTime = millis();
-  delay(1000);
+void testDataRead() {
+  dataFile = SD.open(SD_DATA_FILE, FILE_READ);
+  while (dataFile.available()) {
+    Serial.write(dataFile.read());
+  }
+  dataFile.close();
 }
 
-
-
-void devicesInit() {
-  initIMU();
-  initSD();
+void removeDataFileIfExists() {
+  if (SD.exists("data.txt")) {
+    SD.remove("data.txt");
+  }
 }
 
 void initIMU() {
@@ -58,28 +62,114 @@ void initIMU() {
   }
 }
 void initAltimeter(){
- 
- }
+  if (!baro.begin()) {
+      Serial.println("No Altimeter Detected... idling");
+      while(1);
+  }
+  baro.setSeaPressure(SEATTLE_BASELINE_ATM);
+}
 
-const char CSV_HEADER[] PROGMEM = {"linAccelX,linAccelY,linAccelZ,gravitAccelX,gravitAccelY,gravitAccelZ,alt,rad"};
+void devicesInit() {
+  initIMU();
+  initAltimeter();
+  initSD();
+}
+
+const char CSV_HEADER[] = {"time,linAccelX,linAccelY,linAccelZ,gravitAccelX,gravitAccelY,gravitAccelZ,alt,rad"};
+
 void initSD() {
-  pinMode(10, OUTPUT);
+  pinMode(SD_PIN, OUTPUT);
   if (!SD.begin(SD_PIN)) {
     Serial.println("SD Card not present!");
     while(1);
   }
 
-  dataFile = SD.open("data.txt", FILE_WRITE);
+  removeDataFileIfExists();
+
+  dataFile = SD.open(SD_DATA_FILE, FILE_WRITE);
   if (!dataFile) {
     Serial.println("Error opening SD data file!");
     while(1);
   }
 
-  
   //write initial header to datafile
   dataFile.println(CSV_HEADER);
+  dataFile.close();
 }
 
+
+long currentTime = 0L;
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pollSensors();
+  checkAltimeter();
+  currentTime = millis();
+  delay(1000);
+}
+
+
+#define PREVIOUS_POINTS 3 
+
+// vectors for gravitational and linear acceleration.
+imu::Vector<3> gravitAccel;
+imu::Vector<3> linearAccel;
+
+double altimeterValues[PREVIOUS_POINTS];
+double alt;
+unsigned int alt_index = 0;
+double radiation = 0;
+
+// do nothing for the moment. This will eventually actuate a servo at a certain altitude;
+
+
+const char SEP[] = {", "};
+
+void pollSensors() {
+  dataFile = SD.open(SD_DATA_FILE, FILE_WRITE);
+  
+	linearAccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  gravitAccel = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+  alt = baro.getAltitude();
+
+  for (int i = 1; i < PREVIOUS_POINTS; i++) {
+    altimeterValues[i] = altimeterValues[i - 1];
+  }
+  altimeterValues[0] = alt;
+
+ /* dataFile.print(currentTime, DEC);
+  dataFile.print(SEP);*/
+  writeCSVPoint(currentTime);
+  
+  writeCSVPoint(linearAccel.x());  
+  writeCSVPoint(linearAccel.y());
+  writeCSVPoint(linearAccel.z());
+  
+  writeCSVPoint(gravitAccel.x());  
+  writeCSVPoint(gravitAccel.y());
+  writeCSVPoint(gravitAccel.z());
+  
+  writeCSVPoint(alt);
+  writeCSVLastPoint(radiation);
+  Serial.println("write finished");
+}
+
+void writeCSVPoint(double data) {
+  dataFile.print(data, DEC);
+  dataFile.print(SEP);
+}
+
+void writeCSVLastPoint(double data) {
+  dataFile.println(data, DEC);
+  dataFile.close();
+}
+
+void checkAltimeter() {
+  return;
+}
+
+/*
 
 // CONSTANTS USEFUL FOR DATA LOGGING
 #define LINE_FMT "%s, %s, %s, %s, %s, %s, %s, %s"
@@ -95,23 +185,6 @@ char line[MAX_LINE_LENGTH];
 char xAccel[BUF_LEN], yAccel[BUF_LEN], zAccel[BUF_LEN], gXAccel[BUF_LEN], gYAccel[BUF_LEN], gZAccel[BUF_LEN];
 char altBuf[BUF_LEN];
 char radBuf[BUF_LEN]; //not used for now.
-
-
-// vectors for gravitational and linear acceleration.
-imu::Vector<3> gravitAccel;
-imu::Vector<3> linearAccel;
-
-#define PREVIOUS_POINTS 3
-
-double altimeterValues[PREVIOUS_POINTS];
-double alt;
-unsigned int alt_index = 0;
-double radiation = 0;
-
-// do nothing for the moment. This will eventually actuate a servo at a certain altitude;
-void checkAltimeter() {
-  return;
-}
 
 void pollSensors() {
   linearAccel = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
@@ -134,13 +207,17 @@ void pollSensors() {
   convertDouble(alt, altBuf, sizeof altBuf);
   convertDouble(radiation, radBuf, sizeof radBuf);
 
-  snprintf(line, sizeof line, LINE_FMT, xAccel, yAccel, zAccel, gXAccel, gYAccel, gZAccel, altBuf, radBuf);
-  dataFile.println(line);
+  Serial.println(linearAccel.x(), DEC);
+  Serial.println(xAccel);
+  Serial.println(gravitAccel.z());
+
+  int n = snprintf(line, sizeof line, LINE_FMT, xAccel, yAccel, zAccel, gXAccel, gYAccel, gZAccel, altBuf, radBuf);
+  Serial.println(line);
 }
 
 void convertDouble(double d, char *buf, size_t buf_len) {
   if (!buf) return;
-  dtostrf(d, buf_len - 1, buf_len - 3, buf); 
+  dtostrf(d, buf_len-1, 4, buf); 
   buf[buf_len-1] = '\0';
-}
+}*/
 
